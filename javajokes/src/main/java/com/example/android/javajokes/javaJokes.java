48530f7e0b16c2cff3da5be17d928650d93ab89e
Lucasv94 <lucasvasquez94@gmail.com>
2017-02-19 23:23:51 -0800

    Add basic java library

---
+++ javajokes/src/main/java/com/example/android/javajokes/javaJokes.java
@@ -0,0 +1,11 @@
+package com.example.android.javajokes;
+
+/**
+ * Created by LucasVasquez on 2/19/17.
+ */
+
+public class javaJokes {
+    public static String getJoke() {
+        return "glorious joke";
+    }
+}
